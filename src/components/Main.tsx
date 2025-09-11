b6a1ead0a9cf6cd5b55a80ec45c27a55ec9d855b
jvcbyte <jvc8463@gmail.com>
2025-09-11 17:43:23 +0100

    refactor: replace BrowserRouter with  HashRouter

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import SideBar from "./Sidebar";
 import Dashboard from "./Dashboard";
