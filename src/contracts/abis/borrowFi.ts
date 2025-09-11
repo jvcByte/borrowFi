5d2a04719b69c79068784159dd7019a05def70ca
jvcbyte <jvc8463@gmail.com>
2025-09-11 22:37:57 +0100

    update abi

--- src/contracts/abis/borrowFi.ts
+++ src/contracts/abis/borrowFi.ts
@@ -2,100 +2,148 @@ export default [
   {
     inputs: [
       { internalType: "address", name: "_bFI", type: "address" },
-      { internalType: "address", name: "_clt", type: "address" },
+      { internalType: "address", name: "_clt", type: "address" }
     ],
     stateMutability: "nonpayable",
-    type: "constructor",
+    type: "constructor"
+  },
+  {
+    inputs: [{ internalType: "address", name: "token", type: "address" }],
+    name: "SafeERC20FailedOperation",
+    type: "error"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "user", type: "address" },
+      { indexed: false, internalType: "uint256", name: "amount", type: "uint256" }
+    ],
+    name: "Borrowed",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "user", type: "address" },
+      { indexed: false, internalType: "uint256", name: "amount", type: "uint256" }
+    ],
+    name: "CollateralAdded",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "user", type: "address" },
+      { indexed: false, internalType: "uint256", name: "amount", type: "uint256" }
+    ],
+    name: "CollateralWithdrawn",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "user", type: "address" },
+      { indexed: false, internalType: "uint256", name: "amount", type: "uint256" }
+    ],
+    name: "Repaid",
+    type: "event"
   },
   {
     inputs: [],
     name: "DECIMALS",
     outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
-    type: "function",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "HEALTHY_THRESHOLD",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function"
   },
   {
     inputs: [{ internalType: "uint256", name: "amount", type: "uint256" }],
     name: "addCollateral",
     outputs: [],
     stateMutability: "nonpayable",
-    type: "function",
+    type: "function"
   },
   {
     inputs: [],
     name: "bFI",
     outputs: [{ internalType: "contract IERC20", name: "", type: "address" }],
     stateMutability: "view",
-    type: "function",
+    type: "function"
   },
   {
     inputs: [{ internalType: "uint256", name: "amount", type: "uint256" }],
     name: "borrow",
     outputs: [],
     stateMutability: "nonpayable",
-    type: "function",
+    type: "function"
   },
   {
     inputs: [],
     name: "clt",
     outputs: [{ internalType: "contract IERC20", name: "", type: "address" }],
     stateMutability: "view",
-    type: "function",
+    type: "function"
   },
   {
-    inputs: [{ internalType: "address", name: "user", type: "address" }],
+    inputs: [{ internalType: "address", name: "", type: "address" }],
     name: "collateralOf",
-    outputs: [{ internalType: "uint256", name: "amount", type: "uint256" }],
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
-    type: "function",
+    type: "function"
   },
   {
     inputs: [],
     name: "getLTC",
     outputs: [{ internalType: "uint256", name: "ratio", type: "uint256" }],
     stateMutability: "view",
-    type: "function",
+    type: "function"
   },
   {
     inputs: [],
     name: "isHealthy",
     outputs: [{ internalType: "bool", name: "", type: "bool" }],
     stateMutability: "view",
-    type: "function",
+    type: "function"
   },
   {
-    inputs: [{ internalType: "address", name: "user", type: "address" }],
+    inputs: [{ internalType: "address", name: "", type: "address" }],
     name: "loanOf",
-    outputs: [{ internalType: "uint256", name: "amount", type: "uint256" }],
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
-    type: "function",
+    type: "function"
   },
   {
     inputs: [{ internalType: "uint256", name: "amount", type: "uint256" }],
     name: "repay",
     outputs: [],
     stateMutability: "nonpayable",
-    type: "function",
+    type: "function"
   },
   {
     inputs: [],
     name: "totalBorrowed",
     outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
-    type: "function",
+    type: "function"
   },
   {
     inputs: [],
     name: "totalCollateral",
     outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
-    type: "function",
+    type: "function"
   },
   {
     inputs: [{ internalType: "uint256", name: "amount", type: "uint256" }],
     name: "withdrawCollateral",
     outputs: [],
     stateMutability: "nonpayable",
-    type: "function",
-  },
+    type: "function"
+  }
 ] as const;
